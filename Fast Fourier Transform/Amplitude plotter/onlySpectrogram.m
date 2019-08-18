a54eb9504e4ec66ba286a04244b3bd28a917e817
alexfcoding <fedotov1609@gmail.com>
2019-08-18 20:55:01 +0600

    Kaiser params + figures

--- Fast Fourier Transform/Amplitude plotter/onlySpectrogram.m
+++ Fast Fourier Transform/Amplitude plotter/onlySpectrogram.m
@@ -1,3 +1,4 @@
+lengthSig = 500;
 figure
 subplot(3,1,1);
 audioin1 = 0;
@@ -19,11 +20,16 @@ plot(aud(1:lengthSig), audioin3(1:lengthSig));
 
 size = 512;
 shift = 512;
+kaiserLength = 200;
+kaiserOverlap = 191;
+spectogramAnglex = 81;
+spectogramAngley = 31;
+
 subplot(3,3,1);
 spectrogram(audioin1(1:512),kaiser(kaiserLength,15),kaiserOverlap,1:5:10000,FFS,'yaxis')
 colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('а) G(t)');
+title('а1) G(t)');
 xlabel('t, мс') 
 ylabel('w, к√ц') 
 zlabel('A, дЅ') 
@@ -32,7 +38,7 @@ subplot(3,3,4);
 spectrogram(audioin2(1:size),kaiser(kaiserLength,15),kaiserOverlap,1:5:10000,FFS,'yaxis')
 colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('б) V(t)');
+title('б1) V(t)');
 xlabel('t, мс') 
 ylabel('w, к√ц') 
 zlabel('A, дЅ') 
@@ -41,7 +47,7 @@ subplot(3,3,7);
 spectrogram(audioin3(1:size),kaiser(kaiserLength,15),kaiserOverlap,1:5:10000,FFS,'yaxis')
 colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('в) Y(t)');
+title('в1) Y(t)');
 xlabel('t, мс') 
 ylabel('w, к√ц') 
 zlabel('A, дЅ') 
@@ -52,7 +58,7 @@ subplot(3,3,2);
 spectrogram(audioin1(size:size+shift),kaiser(kaiserLength,15),kaiserOverlap,1:5:10000,FFS,'yaxis')
 colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('г) G(t)');
+title('а2) G(t)');
 xlabel('t, мс') 
 ylabel('w, к√ц') 
 zlabel('A, дЅ') 
@@ -61,7 +67,7 @@ subplot(3,3,5);
 spectrogram(audioin2(size:size+shift),kaiser(kaiserLength,15),kaiserOverlap,1:5:10000,FFS,'yaxis')
 colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('д) V(t))');
+title('б2) V(t))');
 xlabel('t, мс') 
 ylabel('w, к√ц') 
 zlabel('A, дЅ') 
@@ -70,7 +76,7 @@ subplot(3,3,8);
 spectrogram(audioin3(size:size+shift),kaiser(kaiserLength,15),kaiserOverlap,1:5:10000,FFS,'yaxis')
 colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('е) Y(t)');
+title('в2) Y(t)');
 xlabel('t, мс') 
 ylabel('w, к√ц') 
 zlabel('A, дЅ') 
@@ -81,7 +87,7 @@ subplot(3,3,3);
 spectrogram(audioin1(size:size+shift),kaiser(kaiserLength,15),kaiserOverlap,1:5:10000,FFS,'yaxis')
 colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('ж) G(t)');
+title('а3) G(t)');
 xlabel('t, мс') 
 ylabel('w, к√ц') 
 zlabel('A, дЅ') 
@@ -90,7 +96,7 @@ subplot(3,3,6);
 spectrogram(audioin2(size:size+shift),kaiser(kaiserLength,15),kaiserOverlap,1:5:10000,FFS,'yaxis')
 colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('з) V(t)');
+title('б3) V(t)');
 xlabel('t, мс') 
 ylabel('w, к√ц') 
 zlabel('A, дЅ') 
@@ -99,7 +105,7 @@ subplot(3,3,9);
 spectrogram(audioin3(size:size+shift),kaiser(kaiserLength,15),kaiserOverlap,1:5:10000,FFS,'yaxis')
 colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('и) Y(t)');
+title('в3) Y(t)');
 xlabel('t, мс') 
 ylabel('w, к√ц') 
 zlabel('A, дЅ') 
