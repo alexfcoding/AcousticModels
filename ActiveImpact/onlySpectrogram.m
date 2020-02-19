6e19f9ac8d059f96bc231ff28acb91e7bb4f40df
Aleksander Fedotov <46035835+alexfcoding@users.noreply.github.com>
2020-02-20 00:23:19 +0300

    Some experiments

--- ActiveImpact/onlySpectrogram.m
+++ ActiveImpact/onlySpectrogram.m
@@ -1,55 +1,61 @@
-lengthSig = 500;
+lengthSig = 3000;
 figure
 subplot(3,1,1);
 audioin1 = 0;
-[audioin1,FFS] = audioread('Only solenoid.wav');
+[audioin1,FFS] = audioread('Only solenoid22.wav');
 aud = 1:1:length(audioin1);
 plot(aud(1:lengthSig), audioin1(1:lengthSig));
+xlabel('Ќомер отсчета') 
+ylabel('јмплитуда') 
+zlabel('јмплитуда, дЅ') 
 
 subplot(3,1,2);
 audioin2 = 0;
-[audioin2,FFS] = audioread('Solenoid with welding.wav');
+[audioin2,FFS] = audioread('Solenoid with welding22.wav');
 aud = 1:1:length(audioin2);
 plot(aud(1:lengthSig), audioin2(1:lengthSig));
+xlabel('Ќомер отсчета') 
+ylabel('јмплитуда') 
+zlabel('јмплитуда, дЅ') 
 
 subplot(3,1,3);
 audioin3 = 0;
-[audioin3,FFS] = audioread('Solenoid with drilling.wav');
+[audioin3,FFS] = audioread('Solenoid with drilling2222.wav');
 aud = 1:1:length(audioin3);
 plot(aud(1:lengthSig), audioin3(1:lengthSig));
+xlabel('Ќомер отсчета') 
+ylabel('јмплитуда') 
+zlabel('јмплитуда, дЅ') 
 
-size = 3800;
+size = 3000;
 shift = 512;
-kaiserLength = 256;
-kaiserOverlap = 240;
-spectogramAnglex = -146;
-spectogramAngley = 35;
+kaiserLength = 512;
+kaiserOverlap = 501;
+spectogramAnglex = -140;
+spectogramAngley = 45;
 
 figure
 subplot(3,1,1);
-spectrogram(audioin1(1400:size),kaiser(kaiserLength,15),kaiserOverlap,1:5:20000,FFS,'yaxis')
+spectrogram(audioin1(1:size),kaiser(kaiserLength,15),kaiserOverlap,1:5:20000,FFS,'yaxis')
 colormap jet
 view(spectogramAnglex,spectogramAngley);
-title('а1) G(t)');
-xlabel('t, мс') 
-ylabel('w, к√ц') 
-zlabel('A, дЅ') 
+xlabel('¬рем€, мс') 
+ylabel('„астота, к√ц') 
+zlabel('јмплитуда, дЅ') 
 
 subplot(3,1,2);
-spectrogram(audioin2(1400:size),kaiser(kaiserLength,15),kaiserOverlap,1:5:20000,FFS,'yaxis')
+spectrogram(audioin2(1:size),kaiser(kaiserLength,15),kaiserOverlap,1:5:20000,FFS,'yaxis')
 %colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('б1) V(t)');
-xlabel('t, мс') 
-ylabel('w, к√ц') 
-zlabel('A, дЅ') 
+xlabel('¬рем€, мс') 
+ylabel('„астота, к√ц') 
+zlabel('јмплитуда, дЅ') 
 
 subplot(3,1,3);
-spectrogram(audioin3(1400:size),kaiser(kaiserLength,15),kaiserOverlap,1:5:20000,FFS,'yaxis')
+spectrogram(audioin3(1:size),kaiser(kaiserLength,15),kaiserOverlap,1:5:20000,FFS,'yaxis')
 %colormap hsv
 view(spectogramAnglex,spectogramAngley);
-title('в1) Y(t)');
-xlabel('t, мс') 
-ylabel('w, к√ц') 
-zlabel('A, дЅ') 
+xlabel('¬рем€, мс') 
+ylabel('„астота, к√ц') 
+zlabel('јмплитуда, дЅ') 
 
