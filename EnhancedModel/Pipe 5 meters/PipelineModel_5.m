29b0cee2e1c6e02fd11b86e6972711c5b722f939
Aleksander Fedotov <46035835+alexfcoding@users.noreply.github.com>
2021-02-19 20:16:50 +0300

    Cleanup

--- EnhancedModel/Pipe 5 meters/PipelineModel_5.m
+++ EnhancedModel/Pipe 5 meters/PipelineModel_5.m
@@ -1,5 +1,3 @@
-clear all
-clc
 h = 0.003;
 p0 = 7800;
 E = 200*10^9;
@@ -10,70 +8,58 @@ A1 = 7;
 A2 = 1;
 A3 = 1500000.*0.7;
 A = 0.038;
-a4 = E.*J./p0./A;
+%a4 = E.*J./p0./A;
 %M = pi*(d/2-h).^2*p0;
 M = p0 * pi * d * h;
 a4 = E.*J./M;
-ZZ = 0;
-N = 1;
+z = 0;
+n = 1;
 x1 = L/9.4;
 x2 = L/2;
 
-for I = 1:N
-    VV = 1;
-    Y = 0;
-    YY = 0;
-    t = 0;
-    i = 1:2:100;
-    nnnn = 1:2:100;
-    Cn = pi/2 * (2 * nnnn+1); 
-    %W = E.*J/M/L.^4.*Cn.^4;
-    Fd = 44100; 
-    Time = 1;
+for k = 1:n    
+    y = 0;
+    y2 = 0;    
+    mod = 1:2:100;
+    %mod = 1:2:100;
+    %Cn = pi/2*(2*mod+1); 
+    %w = E.*J/M/L.^4.*Cn.^4;
+    fd = 44100; 
+    time = 1;
     Tb = 0.00004;
     t = 0.00004;
+    delta = (1./2.*(A1.*mod.^1.*pi.^4./(L.^4)+A2));
+    w =  sqrt(A3+a4.*mod.^4.*pi.^4./(L.^4));   
+    Oi = sqrt(w.^2 - delta.^2);
 
-    delta = (1./2.*(A1.*i.^1.*pi.^4./(L.^4)+A2));
-
-    W =  sqrt(A3+a4.*i.^4.*pi.^4./(L.^4)); % 
-
-    W_MAX = max(W);
-
-    Oi = sqrt(W.^2 - delta.^2);
-
-    % цикл построения сигнала-------------------------------------------------------------------------
-    for HHH = 1:1:(Time*Fd-1) 
-        Y = pi./(M.*L.*Tb).*sum( (-1).^((i-1)./2).*exp(-delta.*t).*sin(i.*pi.*x1./L)./ ...
-        ( (W.^2-pi.^2./(Tb.^2)).^2 + 4.*pi.^2.*delta.^2./(Tb.^2))...
+    % Signal loop -------------------------------------------------------------------------
+    for m = 1:1:(time*fd-1) 
+        y = pi./(M.*L.*Tb).*sum( (-1).^((mod-1)./2).*exp(-delta.*t).*sin(mod.*pi.*x1./L)./ ...
+        ( (w.^2-pi.^2./(Tb.^2)).^2 + 4.*pi.^2.*delta.^2./(Tb.^2))...
         .*((2.*exp(delta.*Tb).*delta.*pi./Tb.*cos(Oi.*Tb) - pi.*exp(delta.*Tb)./Tb./Oi.*...
-        (2.*delta.^2-W.^2+pi.^2./Tb./Tb).*sin(Oi.*Tb)+2.*pi.*delta./Tb).*cos(Oi.*t) + ...
-        (pi.*exp(delta.*Tb)./Tb./Oi.*(2.*delta.^2-W.^2+pi.^2./Tb./Tb).*cos(Oi.*Tb) + ...
-        2.*exp(delta.*Tb).*delta.*pi./Tb.*sin(Oi.*Tb)+pi./Tb./Oi.*(2.*delta.^2-W.^2+pi.^2./Tb./Tb)).* ...
+        (2.*delta.^2-w.^2+pi.^2./Tb./Tb).*sin(Oi.*Tb)+2.*pi.*delta./Tb).*cos(Oi.*t) + ...
+        (pi.*exp(delta.*Tb)./Tb./Oi.*(2.*delta.^2-w.^2+pi.^2./Tb./Tb).*cos(Oi.*Tb) + ...
+        2.*exp(delta.*Tb).*delta.*pi./Tb.*sin(Oi.*Tb)+pi./Tb./Oi.*(2.*delta.^2-w.^2+pi.^2./Tb./Tb)).* ...
         sin(Oi.*t)));
-        t = t + 1/Fd; 
-        YY = [YY Y];
+        t = t + 1/fd; 
+        y2 = [y2 y];
     end
     % ------------------------------------------------------------------------------------------------
-
-    length_YY = length(YY);
-    t = 0:1/Fd:Time-1/Fd; 
-    length_t = length(t);
-    YYYY = YY./max(YY).*0.9;
-    ZZ = [ZZ YYYY];
-    plot (t,YY);    
-    GGG = 0:1/Fd:Time - 1/Fd;
+    
+    t = 0:1/fd:time-1/fd;     
+    y3 = y2./max(y2).*0.9;
+    z = [z y3];
+    plot(t,y2);
 end
 
-t = 0:1/Fd:Time.*N;
-t = 0:1/Fd:Time.*N-1/Fd;
-plot (t,YY); 
+t = 0:1/fd:time*n-1/fd;
+plot (t,y2); 
 xlabel('Time, secs');
 ylabel('Amplitude');
 figure
-spectrogram(YY,kaiser(256,15),203,1024,22050,'yaxis')
+spectrogram(y3,kaiser(256,15),203,1024,22050,'yaxis')
 colormap jet    
 view(113,64)
 colorbar
-audiowrite('WaveModel_5.wav',ZZ,Fd);
 
-
+audiowrite('WaveModel_5.wav',z,fd);
--- SimpleModel/SimplePipelineModel_5.m
+++ SimpleModel/SimplePipelineModel_5.m
@@ -1,62 +1,49 @@
-clear all
-clc
-L=5;
-YY = 0;
+L = 5;
+y2 = 0;
 x2 = 0;
-YYYY = 0;
-abir = 2;
-progress = 0;
-numm  = 1;
-for N = 1:numm
-    progress = progress + 1;
-    x1 = L/7.4; 
-    abir = abir + 0.5;
+n = 1;
+
+for k = 1:n    
+    x1 = L/7.4;     
     x2 = L - L/2000; 
     h = 0.002;
     E = 200*10^9;
     d = 0.07;
-    J=3.14*d^3*h/8;
+    J = 3.14*d^3*h/8;
     p0 = 7800;
     M = pi/4*(d^2-(d-h)^2)*L*p0 ./1.4; 
-    VV = 1; 
-    Y = 0; 
-    t = 0; 
-    iii = 1:1:500; 
-    nnnn = 1:1:500; 
-    Cn = pi/2*(2*nnnn+1); 
-    %Cn = pi*nnnn; % для свободно опертого стержня
-    %Cn = pi*(nnnn+0.1); 
-    W = sqrt(E.*J/M/(L.^4).*Cn.^4);
-    W_MAX = max(W);
-    Fd = 44100;
-    Time = 3;
-    hhh=1;
+    V = 1; 
+    y = 0; 
+    t = 0;     
+    mod = 1:1:500; 
+    Cn = pi/2*(2*mod+1); 
+    %Cn = pi*mod;
+    %Cn = pi*(mod+0.1); 
+    w = sqrt(E.*J/M/(L.^4).*Cn.^4);    
+    fd = 44100;
+    time = 3;    
     damping = 0.0013;
 
-    % Цикл построения сигнала -------------------------------------------------------------
-    for HHH = 1:1:(Time*Fd-1) 
-        Y = sum(2.*VV./L.*(1./W.*sin(iii.*pi.*x2/L).*sin(iii.*pi.*x1./L).*sin(W.*t)).* ...
-        exp(-t.*(W.*damping+1.1)) );
-        t = t + 1/Fd; 
-        YY = [YY Y];
+    % Signal loop -------------------------------------------------------------
+    for m = 1:1:(time*fd-1) 
+        y = sum(2.*V./L.*(1./w.*sin(mod.*pi.*x2/L).*sin(mod.*pi.*x1./L).*sin(w.*t)).* ...
+        exp(-t.*(w.*damping+1.1)) );
+        t = t + 1/fd; 
+        y2 = [y2 y];
     end
     % -------------------------------------------------------------------------------------
-
-    length_YY = length(YY);
-    t = 0:1/Fd:Time-1/Fd;
-    length_t = length(t);
-    LENGTH_YYYY = length(YY);
-    plot (t,YY); 
+    
+    t = 0:1/fd:time-1/fd; 
+    plot (t,y2); 
     xlabel('Time, secs');
     ylabel('Amplitude');
     figure
-    spectrogram(YY,kaiser(256,15),203,1024,22050,'yaxis')
+    spectrogram(y2,kaiser(256,15),203,1024,22050,'yaxis')
     colormap jet    
     view(118,72)
-    colorbar
-    progress/numm*100;
+    colorbar    
     L = L + 2.5;
 end
 
-YY = YY./max(YY);
-audiowrite('WaveModelSimple_5.wav',YY,Fd);
+y2 = y2./max(y2);
+audiowrite('WaveModelSimple_5.wav',y2,fd);
